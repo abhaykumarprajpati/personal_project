438b809af8f1da3091c6dfa61614a5b72973646e
abhaykumarprajapati <abhaya.prajapati@aarvytechnologies.co.in>
2023-10-28 12:21:13 +0530

    first commit use ref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef, useMemo } from "react"
 
 function App() {
+
+  const [toggle, settoggle] = useState(false)
+  console.log(' rendering_toggle')
+  const [count, setcount] = useState(0)
+
+  const increment = () => {
+    console.log(' rendering_count ')
+    setcount(count + 1)
+  }
+
+  const countNumber = (num) => {
+    console.log('rendering_count_inside_countnumber', num)
+    return num;
+  }
+
+  // const checkdata = countNumber(count)
+
+  const checkdata = useEffect(() => {
+    return countNumber(count)
+  }, [count])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>
+        welcome
+      </h3>
+
+      <button onClick={increment}>add</button>
+      <p>{`count is ${count}`}</p>
+      <p>{`check data count is ${checkdata}`}</p>
+      <button onClick={() => settoggle(!toggle)}>
+        {toggle ? "click me again" : "click me"}
+      </button>
+
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
